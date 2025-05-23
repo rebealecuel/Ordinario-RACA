279dd22a64d873de4b278cf3c00be471198c26d9
Lab2-12 <Lab2-12@LAB2-12>
2025-05-22 18:38:01 -0600

    correo

---
+++ Correo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordinario_RACA
+{
+    internal class Correo
+    {
+        public void EnviarCorreo(string error) 
+        {
+            try
+            {
+
+            }
+            catch (Exception ex) 
+            {
+                
+            }
+        }
+    }
+}
