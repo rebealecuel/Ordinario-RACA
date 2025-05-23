54d2a753f5c6e12fde01d838b2845ef691450940
Lab2-12 <Lab2-12@LAB2-12>
2025-05-22 18:11:40 -0600

    sd

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,21 @@
         {
             this.DGVMostrar = new System.Windows.Forms.DataGridView();
             this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.txbMarca = new System.Windows.Forms.TextBox();
+            this.lblModelo = new System.Windows.Forms.Label();
+            this.txbModelo = new System.Windows.Forms.TextBox();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.txbYear = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txbPrecio = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txbID = new System.Windows.Forms.TextBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.txbEstado = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.DGVMostrar)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,24 +58,163 @@
             // 
             // btnMostrar
             // 
-            this.btnMostrar.Location = new System.Drawing.Point(72, 228);
+            this.btnMostrar.Location = new System.Drawing.Point(158, 275);
             this.btnMostrar.Name = "btnMostrar";
             this.btnMostrar.Size = new System.Drawing.Size(98, 36);
             this.btnMostrar.TabIndex = 1;
             this.btnMostrar.Text = "Mostrar";
             this.btnMostrar.UseVisualStyleBackColor = true;
             // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(387, 392);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(98, 36);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(516, 392);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(98, 36);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(641, 392);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(98, 36);
+            this.btnActualizar.TabIndex = 4;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(540, 232);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblMarca.TabIndex = 6;
+            this.lblMarca.Text = "Marca";
+            // 
+            // txbMarca
+            // 
+            this.txbMarca.Location = new System.Drawing.Point(609, 232);
+            this.txbMarca.Name = "txbMarca";
+            this.txbMarca.Size = new System.Drawing.Size(99, 20);
+            this.txbMarca.TabIndex = 5;
+            // 
+            // lblModelo
+            // 
+            this.lblModelo.AutoSize = true;
+            this.lblModelo.Location = new System.Drawing.Point(540, 258);
+            this.lblModelo.Name = "lblModelo";
+            this.lblModelo.Size = new System.Drawing.Size(42, 13);
+            this.lblModelo.TabIndex = 8;
+            this.lblModelo.Text = "Modelo";
+            // 
+            // txbModelo
+            // 
+            this.txbModelo.Location = new System.Drawing.Point(609, 258);
+            this.txbModelo.Name = "txbModelo";
+            this.txbModelo.Size = new System.Drawing.Size(99, 20);
+            this.txbModelo.TabIndex = 7;
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(540, 284);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(26, 13);
+            this.lblYear.TabIndex = 10;
+            this.lblYear.Text = "Año";
+            // 
+            // txbYear
+            // 
+            this.txbYear.Location = new System.Drawing.Point(609, 284);
+            this.txbYear.Name = "txbYear";
+            this.txbYear.Size = new System.Drawing.Size(99, 20);
+            this.txbYear.TabIndex = 9;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(540, 310);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 12;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // txbPrecio
+            // 
+            this.txbPrecio.Location = new System.Drawing.Point(609, 310);
+            this.txbPrecio.Name = "txbPrecio";
+            this.txbPrecio.Size = new System.Drawing.Size(99, 20);
+            this.txbPrecio.TabIndex = 11;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(540, 206);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 14;
+            this.lblID.Text = "ID";
+            // 
+            // txbID
+            // 
+            this.txbID.Location = new System.Drawing.Point(609, 206);
+            this.txbID.Name = "txbID";
+            this.txbID.Size = new System.Drawing.Size(99, 20);
+            this.txbID.TabIndex = 13;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(540, 336);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(40, 13);
+            this.lblEstado.TabIndex = 16;
+            this.lblEstado.Text = "Estado";
+            // 
+            // txbEstado
+            // 
+            this.txbEstado.Location = new System.Drawing.Point(609, 336);
+            this.txbEstado.Name = "txbEstado";
+            this.txbEstado.Size = new System.Drawing.Size(99, 20);
+            this.txbEstado.TabIndex = 15;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.txbEstado);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txbID);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.txbPrecio);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.txbYear);
+            this.Controls.Add(this.lblModelo);
+            this.Controls.Add(this.txbModelo);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.txbMarca);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.btnMostrar);
             this.Controls.Add(this.DGVMostrar);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.DGVMostrar)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +222,21 @@
 
         private System.Windows.Forms.DataGridView DGVMostrar;
         private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.TextBox txbMarca;
+        private System.Windows.Forms.Label lblModelo;
+        private System.Windows.Forms.TextBox txbModelo;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.TextBox txbYear;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox txbPrecio;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txbID;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.TextBox txbEstado;
     }
 }
 
